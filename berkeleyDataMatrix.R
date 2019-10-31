080109ee45f83314cae99900e71cd926db0b681e
Allison Hayes <ahayes2@zagmail.gonzaga.edu>
2019-10-31 09:32:53 -0700

    list of simulated temperature

--- berkeleyDataMatrix.R
+++ berkeleyDataMatrix.R
@@ -1,10 +1,10 @@
 setwd("/Users/allison/Desktop/Markov_2019_Research")
 dF <- read.csv("year_avg.csv", header = F)
 dF$V1 -> dataList
-len <- length(dataList)
+lenDataList <- length(dataList)
 percentChange <- vector()
 
-for(i in 2:len-1)
+for(i in 2:lenDataList-1)
 {
   cur <- dataList[i]
   nex <- dataList[i+1]
@@ -12,91 +12,46 @@ for(i in 2:len-1)
   percentChange  <- append(percentChange, change)
 }
 
-max <- max(percentChange)
-min <- min(percentChange)
-
-print(max)
-print(min)
-print(percentChange)
-
 binList <- c(-2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3)
 tStates <- cut(percentChange, breaks = binList,
                labels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
-print(tStates)
-
 
 library(markovchain)
 weatherFittedMLE <- markovchainFit(data = tStates)
 weatherFittedMLE
-simulatedWeather <- rmarkovchain(n = 80, object = weatherFittedMLE$estimate, t0 = 2)
+simStates <- rmarkovchain(n = 80, object = weatherFittedMLE$estimate, t0 = 2)
 
-print(simulatedWeather)
-plot(simulatedWeather, type="l")
-
-len <- length(simulatedWeather)
+lenSimStates <- length(simStates) 
 i = 1
+k = 1
 
-estimatedChange <- vector()
-add
+simPercentChange <- vector()
 
-while (i < len)
+while(i <= lenSimStates)
 {
-  if (simulatedWeather[i] == "1")
+  for(k in 1:(length(binList) - 1))
   {
-    add <- (((-2.5 + -2)/2) + 14.178)
-    append(estimatedChange, add)
+    if(simStates[i] == k)
+    {
+      simPercentChange[i] = ((((binList[k] + binList[k+1])/2))/100)+1
+    }
   }
-  if (simulatedWeather[i] == "2")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add) 
-  }
-  if (simulatedWeather[i] == "3")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add) 
-  }
-  if (simulatedWeather[i] == "4")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add)
-  }
-  if (simulatedWeather[i] == "5")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add) 
-  }
-  if (simulatedWeather[i] == "6")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add) 
-  }
-  if (simulatedWeather[i] == "7")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add)
-  }
-  if (simulatedWeather[i] == "8")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add)
-  }
-  if (simulatedWeather[i] == "9")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add)
-  }
-  if (simulatedWeather[i] == "10")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add)
-  }
-  if (simulatedWeather[i] == "11")
-  {
-    add <- (((2 - 2.5)/2) + 14.178)
-    append(estimatedChange, add)
-  }
+  i <- i + 1
 }
 
-#print(estimatedChange)
+curAvgT = 14.178 + 0.77
+
+cumPerChange = prod(simPercentChange)
+print(cumPerChange)
+temp <- vector()
+
+for(i in 0: lenSimStates - 1)
+  temp[i+1] = curAvgT * (simPercentChange[i+1])^1
+
+
+print(simPercentChange)
+print(temp)
+
+plot(dataList, type = "l")
+lines(temp, type = "l", col = "red")
 
